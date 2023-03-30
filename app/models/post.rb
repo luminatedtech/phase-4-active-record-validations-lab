3ae0b95202b6e623b67e11e85e141469e88c1432
Allen Kim <avalmercafe02@gmail.com>
2023-03-30 14:20:52 -0400

    commit tree 82579db7a59733117eb454eb19472136fbf1c583

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validate :must_have_no_clickbait
+    validates :title, presence: true
+    validates(:content, length: {minimum: 250})
+    validates(:summary, length: {maximum: 250})
+    
+
+
+    def must_have_no_clickbait
+        if title == ("True Facts")
+            errors.add(:title, "No clickbait")
+        end 
+    end 
 end
